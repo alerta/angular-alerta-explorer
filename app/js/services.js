ab865dea59a1b938a48be823308e91bb41369531
Nick Satterly <nick.satterly@guardian.co.uk>
2014-09-23 15:34:54 +0100

    missed an old /api reference

--- app/js/services.js
+++ app/js/services.js
@@ -6,7 +6,7 @@ angular.module('explorer.services', ['config', 'ngResource'])
 
 .factory('Alert', ['$resource', 'config',
   function($resource, config) {
-    return $resource(config.endpoint+'/api/alert/:id', {}, {
+    return $resource(config.endpoint+'/alert/:id', {}, {
       'query':  {method:'GET', url: config.endpoint+'/api/alerts'},
       'save':   {method:'POST'},
       'get':    {method:'GET'}
